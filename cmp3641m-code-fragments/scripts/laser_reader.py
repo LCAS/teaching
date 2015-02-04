62e556e0f2e529f653d01f25ade8751f0e30e99a
Marc Hanheide <marc@hanheide.de>
2015-02-04 20:18:16 +0000

    nicer code

--- cmp3641m-code-fragments/scripts/laser_reader.py
+++ cmp3641m-code-fragments/scripts/laser_reader.py
@@ -4,24 +4,22 @@ import rospy
 
 from sensor_msgs.msg import LaserScan
 
+
 class laser_reader:
 
-  def __init__(self):
+    def __init__(self):
+        self.image_sub = rospy.Subscriber("/turtlebot_1/scan",
+                                          LaserScan,
+                                          self.callback)
 
-    self.image_sub = rospy.Subscriber("/turtlebot_1/scan",LaserScan,self.callback)
-    #self.image_sub = rospy.Subscriber("/turtlebot_1/camera/rgb/image_raw",Image,self.callback)
-
-  def callback(self,data):
-      min_range=data.range_max
-      print len(data.ranges)
-      for v in data.ranges[300:340]:
-          if v < min_range:
-              min_range = v
-      print min_range
-    
+    def callback(self, data):
+        min_range = data.range_max
+        print len(data.ranges)
+        for v in data.ranges[300:340]:
+            if v < min_range:
+                min_range = v
+        print min_range
 
 ic = laser_reader()
 rospy.init_node('laser_reader')
 rospy.spin()
-
-
