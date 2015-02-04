319a9f4a337fbc6d43ccd19377f4fe320764d214
Marc Hanheide <marc@hanheide.net>
2015-02-04 19:23:29 +0000

    hough added

--- cmp3641m-code-fragments/scripts/opencv_intro.py
+++ cmp3641m-code-fragments/scripts/opencv_intro.py
@@ -1,11 +1,16 @@
-from cv2 import namedWindow, imread, imshow, waitKey, destroyAllWindows, blur, Canny, startWindowThread
+from cv2 import namedWindow, imread, imshow
+from cv2 import waitKey, destroyAllWindows, startWindowThread
+from cv2 import blur, Canny
 
+namedWindow("original")
 namedWindow("blur")
 namedWindow("canny")
 
 startWindowThread()
 
 img = imread('../blofeld.jpg')
+imshow("original", img)
+
 img2 = blur(img,(3,3))
 imshow("blur", img2)
 
