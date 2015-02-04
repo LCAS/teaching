ddb16a2743e27595e97b213ea616f5d874b9e192
Marc Hanheide <marc@hanheide.de>
2015-02-04 19:25:17 +0000

    formatting

--- cmp3641m-code-fragments/scripts/opencv_intro.py
+++ cmp3641m-code-fragments/scripts/opencv_intro.py
@@ -1,4 +1,6 @@
-from cv2 import namedWindow, imread, imshow, waitKey, destroyAllWindows, blur, Canny, startWindowThread
+
+from cv2 import namedWindow, imread, imshow, waitKey
+from cv2 import destroyAllWindows, blur, Canny, startWindowThread
 
 namedWindow("blur")
 namedWindow("canny")
@@ -6,10 +8,10 @@ namedWindow("canny")
 startWindowThread()
 
 img = imread('../blofeld.jpg')
-img2 = blur(img,(3,3))
+img2 = blur(img, (3, 3))
 imshow("blur", img2)
 
-img3 = Canny(img,10,200)
+img3 = Canny(img, 10, 200)
 imshow("canny", img3)
 
 
