0e38edf9bbd9391d55c9e7741576900b79c77808
Marc Hanheide <marc@hanheide.net>
2015-02-04 17:52:21 +0000

    a very simple opencv demo

---
+++ cmp3641m-code-fragments/scripts/opencv_intro.py
@@ -0,0 +1,17 @@
+from cv2 import namedWindow, imread, imshow, waitKey, destroyAllWindows, blur, Canny, startWindowThread
+
+namedWindow("blur")
+namedWindow("canny")
+
+startWindowThread()
+
+img = imread('../blofeld.jpg')
+img2 = blur(img,(3,3))
+imshow("blur", img2)
+
+img3 = Canny(img,10,200)
+imshow("canny", img3)
+
+
+waitKey(0)
+destroyAllWindows()
