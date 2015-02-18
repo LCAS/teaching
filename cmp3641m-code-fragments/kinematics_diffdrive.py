4582fbf62fd8b80f40aa69ae2c9c4e61cdabe926
Marc Hanheide <marc@hanheide.net>
2015-02-18 19:21:31 +0000

    comments and twist added

--- cmp3641m-code-fragments/kinematics_diffdrive.py
+++ cmp3641m-code-fragments/kinematics_diffdrive.py
@@ -1,11 +1,13 @@
 import math
+from geometry_msgs.msg import Twist
 
 wheel_radius = 1 / (math.pi * 2.0)
-robot_radius = 0.5
+robot_radius = 1
 
 wheel_circumference = 2 * math.pi * wheel_radius
 
 
+# computing the forward kinematics for a differential drive
 def forward_kinematics(w_l, w_r):
     c_l = wheel_circumference * w_l
     c_r = wheel_circumference * w_r
@@ -14,6 +16,7 @@ def forward_kinematics(w_l, w_r):
     return (v, a)
 
 
+# computing the inverse kinematics for a differential drive
 def inverse_kinematics(v, a):
     c_l = v + (robot_radius * a) / 2
     c_r = v - (robot_radius * a) / 2
@@ -22,8 +25,21 @@ def inverse_kinematics(v, a):
     return (w_l, w_r)
 
 
-(w_l, w_r) = inverse_kinematics(1.0, 2 * math.pi)
+# inverse kinematics from a Twist message (This is what a ROS robot has to do)
+def inverse_kinematics_from_twist(t):
+    return inverse_kinematics(t.linear.x, t.angular.z)
+
+
+(w_l, w_r) = inverse_kinematics(0.0, 2 * math.pi)
 print "w_l = %f,\tw_r = %f" % (w_l, w_r)
 
 (v, a) = forward_kinematics(w_l, w_r)
 print "v = %f,\ta = %f" % (v, a)
+
+t = Twist()
+
+t.linear.x = 0.0
+t.angular.z = math.pi
+
+(w_l, w_r) = inverse_kinematics_from_twist(t)
+print "w_l = %f,\tw_r = %f" % (w_l, w_r)
