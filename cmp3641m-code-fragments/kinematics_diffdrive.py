afc99c7c0caf63c203ccc6a9e78f6da1aec324c9
Marc Hanheide <marc@hanheide.de>
2018-02-16 09:24:31 +0000

    fix for diffdrive

--- cmp3641m-code-fragments/kinematics_diffdrive.py
+++ cmp3641m-code-fragments/kinematics_diffdrive.py
@@ -1,7 +1,5 @@
-import math
-# from geometry_msgs.msg import Twist
 
-wheel_radius = 1
+wheel_radius = .1
 robot_radius = 1
 
 
@@ -16,8 +14,8 @@ def forward_kinematics(w_l, w_r):
 
 # computing the inverse kinematics for a differential drive
 def inverse_kinematics(v, a):
-    c_l = v - (robot_radius * a) 
-    c_r = v + (robot_radius * a) 
+    c_l = v - (robot_radius * a)
+    c_r = v + (robot_radius * a)
     w_l = c_l / wheel_radius
     w_r = c_r / wheel_radius
     return (w_l, w_r)
@@ -27,17 +25,19 @@ def inverse_kinematics(v, a):
 def inverse_kinematics_from_twist(t):
     return inverse_kinematics(t.linear.x, t.angular.z)
 
+if __name__ == "__main__":
 
-(w_l, w_r) = inverse_kinematics(0.0, 1.0)
-print "w_l = %f,\tw_r = %f" % (w_l, w_r)
+    (w_l, w_r) = inverse_kinematics(0.0, 1.0)
+    print "w_l = %f,\tw_r = %f" % (w_l, w_r)
 
-(v, a) = forward_kinematics(w_l, w_r)
-print "v = %f,\ta = %f" % (v, a)
+    (v, a) = forward_kinematics(w_l, w_r)
+    print "v = %f,\ta = %f" % (v, a)
 
-# t = Twist()
+    from geometry_msgs.msg import Twist
+    t = Twist()
 
-# t.linear.x = 0.3
-# t.angular.z = 0.8
+    t.linear.x = 0.3
+    t.angular.z = 0.8
 
-# (w_l, w_r) = inverse_kinematics_from_twist(t)
-# print "w_l = %f,\tw_r = %f" % (w_l, w_r)
+    (w_l, w_r) = inverse_kinematics_from_twist(t)
+    print "w_l = %f,\tw_r = %f" % (w_l, w_r)
