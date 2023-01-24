36cbf7aa4d70cced46bbae1d85641a1182332008
Marc Hanheide <marc@hanheide.net>
2023-01-24 14:30:10 +0000

    first working humble launch file and move_square

--- cmp3103m-code-fragments/scripts/move_square.py
+++ cmp3103m-code-fragments/scripts/move_square.py
@@ -1,81 +1,55 @@
 #!/usr/bin/env python
 
-'''
-Copyright (c) 2015, Mark Silliman
-All rights reserved.
+# An example of TurtleBot 3 drawing a 1 meter square.
+# Written for humble
 
-Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+import rclpy
+from rclpy.node import Node
 
-1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-'''
-
-# An example of TurtleBot 2 drawing a 0.4 meter square.
-# Written for indigo
-
-import rospy
 from geometry_msgs.msg import Twist
 from math import radians
 
-class DrawASquare():
+class DrawASquare(Node):
     def __init__(self):
-        # initiliaze
-        rospy.init_node('drawasquare', anonymous=False)
+        super().__init__('drawasquare')        
+        self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.count = 0
+        self.loops = 30 # 30 loops at 10 Hz, takes around 3s
 
-        # What to do you ctrl + c    
-        rospy.on_shutdown(self.shutdown)
-        
-        self.cmd_vel = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size=10)
-     
-	# 5 HZ
-        r = rospy.Rate(5);
-
-	# create two different Twist() variables.  One for moving forward.  One for turning 45 degrees.
+    	# create two different Twist() variables.  One for moving forward.  One for turning 45 degrees.
 
         # let's go forward at 0.2 m/s
-        move_cmd = Twist()
-        move_cmd.linear.x = 0.2
-	# by default angular.z is 0 so setting this isn't required
+        self.move_cmd = Twist()
+        self.move_cmd.linear.x = 1.0/3.0
+	    # by default angular.z is 0 so setting this isn't required
 
         #let's turn at 45 deg/s
-        turn_cmd = Twist()
-        turn_cmd.linear.x = 0
-        turn_cmd.angular.z = radians(45); #45 deg/s in radians/s
+        self.turn_cmd = Twist()
+        self.turn_cmd.linear.x = 0.0
+        self.turn_cmd.angular.z = radians(90/3); #30 deg/s in radians/s
 
-	#two keep drawing squares.  Go forward for 2 seconds (10 x 5 HZ) then turn for 2 second
-        count = 0
-        while not rospy.is_shutdown():
+    def timer_callback(self):
+        #two keep drawing squares.  Go forward for 2 seconds (10 x 5 HZ) then turn for 2 second
 	    # go forward 1 m (5 seconds * 0.2 m / seconds)
-	    rospy.loginfo("Going Straight")
-            for x in range(0,25):
-                self.cmd_vel.publish(move_cmd)
-                r.sleep()
-	    # turn 90 degrees
-	    rospy.loginfo("Turning")
-            for x in range(0,10):
-                self.cmd_vel.publish(turn_cmd)
-                r.sleep()            
-	    count = count + 1
-	    if(count == 4): 
-                count = 0
-	    if(count == 0): 
-                rospy.loginfo("TurtleBot should be close to the original starting position (but it's probably way off)")
-        
-    def shutdown(self):
-        # stop turtlebot
-        rospy.loginfo("Stop Drawing Squares")
-        self.cmd_vel.publish(Twist())
-        rospy.sleep(1)
+        if (self.count // self.loops) % 2 == 0: # Even number
+            self.get_logger().info("Going Straight")
+            self.cmd_vel_pub.publish(self.move_cmd)
+        else:
+            self.get_logger().info("Turning")
+            self.cmd_vel_pub.publish(self.turn_cmd)
+        self.count += 1
+        self.get_logger().info('count: %d' % self.count)
  
 if __name__ == '__main__':
+    rclpy.init()
     try:
-        DrawASquare()
-    except:
-        rospy.loginfo("node terminated.")
+        node = DrawASquare()
+        rclpy.spin(node)
+        node.destroy_node()
+        rclpy.shutdown()
+
+    except Exception as e:
+        print("node terminated. %s" , e)
 
