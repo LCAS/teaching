87c11c9a04c0f53ca45658bfb622a5fb111022cb
Marc Hanheide <marc@hanheide.net>
2024-01-28 12:22:55 +0000

    fix autocomplete and ddsrouter installation

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -5,14 +5,14 @@ set -e
 source /opt/ros/humble/setup.bash
 apt update
 rosdep --rosdistro=humble update 
-
+pip install -U argcomplete
 
 rm -rf /opt/ros/lcas
 mkdir -p /opt/ros/lcas/src
 chown -R lcas /opt/ros/lcas
 cd /opt/ros/lcas/src
 vcs import < /tmp/.devcontainer/lcas.repos
-rosdep install --from-paths . -i -y
+rosdep install --from-paths . -r -i -y
 cd /opt/ros/lcas
 colcon build
 
