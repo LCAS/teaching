589fca57928ea9f433479b655d779df30c502d08
GPrathap <ggeesara@gmail.com>
2024-02-15 08:06:53 +0000

    adding task for week 4

---
+++ control_strategy/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'control_strategy'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Geesara',
+    maintainer_email='ggeesara@gmail.com',
+    description='Basic Diff drive robot control',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'control_strategy = control_strategy.robot_control_strategy:main',
+        ],
+    },
+)
