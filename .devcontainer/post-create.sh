7b9d02765dffccecd46cf7d4adfb8d6f1a5f0071
Marc Hanheide <marc@hanheide.net>
2024-12-11 12:53:57 +0000

    improved documentation

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,9 +5,10 @@ source /opt/ros/lcas/install/setup.bash
 
 # ensure students always have the latest index of everything
 sudo apt update
-rosdep --rosdistro=humble update
+rosdep --rosdistro=${ROS_DISTRO} update
 
 # ensure the additional packages are always sources:
 if ! grep -q "source /opt/ros/lcas/install/setup.bash" ~/.bashrc; then
     echo "source /opt/ros/lcas/install/setup.bash" >> ~/.bashrc
 fi
+
